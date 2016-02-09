1dfe1e42b2f8fddb10532ea073ceab6387666c8e
sbrennion <sebastien.brennion@leanbi.ch>
2016-02-09 11:21:24 +0100

    Initial commit

---
+++ jedox_auto_dockerize.py
@@ -0,0 +1,112 @@
+from jedox_auto_installer import *
+import subprocess
+from docker import Client
+import json
+import os
+from time import sleep
+from string import Template
+
+class dockerizer(default_logger):
+    def __init__(self,args):
+        default_logger.__init__(self,"dockerizer")
+        self.args=args
+        self.config=self.get_config()
+        self.directory=(os.path.dirname(os.path.realpath(__file__)))
+
+        self.base_image_name=args["base_image"]
+        self.docker=Client(base_url='unix://var/run/docker.sock')
+
+
+        self.installer=jedox_installer(args)
+        self.installer.start()
+        sleep(15)
+        self.installer.stop()
+        sleep(15)
+        self.patch()
+        self.add()
+        self.build_base_image(self.base_image_name)
+        self.base_container=self.docker.create_container(self.base_image_name)
+        self.docker.start(self.base_container)
+        self.docker_exec(self.base_container,self.config["docker"]["exec"])
+        self.commit(self.args["docker_repository"],self.args["docker_tag"])
+
+    def get_config(self):
+        try :
+            config_file=self.args["config"]
+            version=self.args["jedox_version"]
+
+            j=json.load(open(config_file))
+            return j[version]
+
+        except KeyError as e:
+            self.logger.exception(e)
+            self.logger.error("Could not find the right config for version=%s in file=%s \n Aborting..." % (version,config_file))
+            sys.exit(1)
+
+    def patch(self):
+        self.logger.info("patching files from installer")
+        self.change_working_directory("patch")
+
+        for p in self.config["patch"]:
+            target=os.path.join(self.args["jedox_home"],p["target"])
+            description=p.get("description",p["target"])
+
+            self.logger.info("patching : %s" % description)
+            subprocess.run("patch %s < %s" % (target,p["source"]),shell=True)
+
+    def add(self):
+        self.logger.info("adding additional content to installation")
+        self.change_working_directory("add")
+
+        for a in self.config["add"]:
+            target=os.path.join(self.args["jedox_home"],a["target"])
+            self.logger.info("copy %s to %s" % (a["source"],target))
+            shutil.copy(a["source"],target)
+
+    def change_working_directory(self,area):
+        working_directory=os.path.join(self.directory,area,self.args["jedox_version"])
+        self.logger.info("working dir is now %s" % working_directory)
+        os.chdir(working_directory)
+
+    def build_base_image(self,image_name="jedox/base"):
+        os.chdir(self.args["jedox_home"])
+        self.logger.info("Import Jedox Suite into intermediate docker image '%s'" % image_name)
+        subprocess.call("""tar --to-stdout --numeric-owner --exclude=/proc --exclude=/sys --exclude='*.tar.gz' --exclude='*.log' -c ./ | docker import --change "CMD while true; do ping 8.8.8.8; done" --change "ENV TERM=xterm" - %s""" % image_name, shell=True)
+        self.logger.info("successfully create basecontainer %s" % image_name)
+
+
+    def docker_exec(self,myContainer,exec_list):
+
+        self.docker.timeout=300
+        for e in exec_list:
+            if "description" in e : #print description in logs if available
+                self.logger.info(e["description"])
+            exec_c=self.docker.exec_create(myContainer,e["cmd"])
+            output=self.docker.exec_start(exec_c)
+
+        self.logger.debug("all exec done")
+
+    def commit(self,repository,tag):
+        tag=Template(self.args["docker_tag"]).safe_substitute(jedox_version=self.args["jedox_version"])
+        self.logger.info("commiting finale image %s to %s : %s" % (self.base_container,repository,tag))
+
+        config={"CMD":"/entrypoint",
+                "EXPOSE": "[80,7777]",
+                }
+        self.docker.commit(self.base_container,repository,tag,conf=config)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='Build a jedox image for docker')
+    parser.add_argument('--installer-download', help='download the installer rather than using a local one',default=False)
+    parser.add_argument('--installer-directory',type=str, help='where the install files are or will be uncompressed',default="/opt/jedox_installation")
+    parser.add_argument('--jedox-home',type=str, help='directory where jedox is installed default=/opt/jedox/ps',default="/opt/jedox/ps")
+    parser.add_argument('--jedox-version', help='Jedox version to be installed ex: 6.0_SR1',default="6.0_SR1")
+    parser.add_argument('--base-image', help='name of the docker base image to be created default=jedox/base',default="jedox/base")
+    parser.add_argument('--config', help='json config file',default="./config.json")
+    parser.add_argument('--docker-repository', help='docker repository where the image will be stored',default="leanbi/jedox6")
+    parser.add_argument('--docker-tag', help='tag used for storing final docker image',default="$jedox_version")
+    args = vars(parser.parse_args())
+
+    installer=dockerizer(args)
---
+++ jedox_auto_installer.py
@@ -0,0 +1,135 @@
+import logging
+import argparse
+import wget
+import sys
+import tarfile
+import os
+import pexpect
+import subprocess
+import shutil
+
+class default_logger:
+    def __init__(self,name,**kwargs):
+        logger=logging.getLogger(name)
+        level=kwargs.get("level","DEBUG")
+        logger.setLevel(getattr(logging,level))
+        formater=logging.Formatter("%(asctime)15s %(name)5s %(levelname)8s %(lineno)3d %(funcName)15s %(message)s")
+        sh=logging.StreamHandler()
+        sh.setFormatter(formater)
+        logger.addHandler(sh)
+        self.logger=logger
+
+
+
+class jedox_installer(default_logger):
+    def __init__(self,args):
+        default_logger.__init__(self,"jedox_installer")
+        self.args=args
+        self.download_directory=self.args["installer_directory"]
+        self.installer_directory=self.args["installer_directory"] + "/" + self.args["jedox_version"] + "/"
+
+        if args["installer_download"]!=False:
+            self.installer=self.download()
+            self.uncompress()
+        self.install()
+
+    def remove_old_install(self):
+        install_dir="/opt/jedox/ps"
+        if os.path.isdir(install_dir):
+            self.logger.warn("older installation detected : stopping and deleting...")
+            try:
+                self.stop()
+            except Exception:
+                self.logger.info("Could not stop jedox, probably not running")
+            shutil.rmtree(install_dir)
+
+
+    def download(self):
+
+        url=self.args["installer_download"]
+        self.logger.info("start downloading installer at : %s" % url)
+
+        if not os.path.isdir(self.download_directory):
+            os.mkdir(self.download_directory)
+        installer_file = wget.download(url,self.download_directory)
+        self.logger.info("saved as : %s" % installer_file)
+        return installer_file
+
+    def uncompress(self):
+        """
+        # delete directory content if not empty
+        if os.path.isdir(self.installer_directory):
+        """
+        if not os.path.isdir(self.installer_directory):
+            os.mkdir(self.installer_directory)
+        self.logger.info("uncompress file %s to %s " % (self.installer,self.installer_directory))
+        os.chdir(self.installer_directory)
+        tar=tarfile.open(self.installer)
+        tar.extractall()
+        tar.close()
+
+    def install(self):
+        self.remove_old_install()
+        os.chdir(self.installer_directory)
+        install_script=os.path.join(self.installer_directory,"install.sh")
+        self.logger.info("starting install script : %s" % install_script)
+        if not os.path.isfile(install_script):
+            self.logger.critical("installer not found, aborting")
+            sys.exit(1)
+
+        else :
+            answers=[
+                {"expect":"Default.*", "answer":"", "description":"jedox_home [/opt/jedox/ps]"},
+                {"expect":"The directory.*", "answer":"y", "description":"create jedox_home directory [Y]"},
+                {"expect":"Default.*", "answer":"jedoxweb", "description":"user for jedox-suite [jedoxweb]"},
+                {"expect":"Default.*", "answer":"jedoxweb", "description":"group for jedox-suite [jedoxweb]"},
+                {"expect":"What is this servers name ?.*", "answer":"jedox_ps", "description":"servers name", "timeout":60 },
+                {"expect":"What is this servers IP-Address ? .*", "answer":"", "description":"servers IP-Address"},
+                {"expect":"Who should get administrative e-mails regarding this server ?.*", "answer":"webmaster@email.com", "description":"admin email"},
+                {"expect":"Which IP-address should the OLAP server run on.*", "answer":"all", "description":"olap ip"},
+                {"expect":"Which port should the OLAP server run on ?.*", "answer":"7777", "description":"olap port"},
+                {"expect":"Which IP-address should the HTTP server listen on.*", "answer": "all", "description":"http ip"},
+                {"expect":"Which port should the HTTP server run on ?.*", "answer":"80", "description":"http port"},
+                {"expect":"Which IP-address should the Spreadsheet server run on.*", "answer":"127.0.0.1", "description":"spreadsheet server ip"},
+                {"expect":"Which port should the Spreadsheet server run on ?.*", "answer":"8193", "description":"spreadsheet server port"},
+                {"expect":"Which AJP-address should the Tomcat server run on ?.*", "answer":"127.0.0.1", "description":"tomcat ajp ip"},
+                {"expect":"Which AJP port should the Tomcat server run on ?.*", "answer":"8010", "description":"tomcat ajp port"},
+                {"expect":"Which HTTP-address should the Tomcat server run on ?.*", "answer":"127.0.0.1", "description":"tomcat http ip"},
+                {"expect":"Which HTTP port should the Tomcat server run on ?.*", "answer":"7775", "description":"tomcat http port"},
+                     ]
+
+
+            child = pexpect.spawn(install_script)
+            fout_file="installer_fout.log"
+            fout = open(fout_file, "ab")
+            child.logfile = fout
+            i=0
+            for a in answers:
+                self.logger.info("awaiting question[%d]:%s expect=%s answer=%s timeout=%d" % (i,a["description"],a["expect"],a["answer"],a.get("timeout",-1)))
+                child.expect (a["expect"],a.get("timeout",-1))
+                child.sendline (a["answer"])
+                i+=1
+            child.expect(pexpect.EOF)
+            fout.close()
+            #self.logger.info(open(fout_file).read())
+            self.logger.info("install finished output below")
+
+    def start(self):
+        subprocess.run(["/opt/jedox/ps/jedox-suite.sh","start"], shell=False, check=True)
+        #Starting httpd: [  OK  ]
+
+    def stop(self):
+        subprocess.run(["/opt/jedox/ps/jedox-suite.sh","stop"], shell=False, check=True)
+        #Unmounting /opt/jedox/ps/sys...done.
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Build a jedox image for docker')
+    parser.add_argument('--installer-download', help='download the installer rather than using a local one',default=False)
+    parser.add_argument('--installer-directory',type=str, help='where the install files are or will be uncompressed',default="/opt/jedox_installation")
+    parser.add_argument('--jedox-version', help='Jedox version to be installed ex: 6.0_SR1',default="6.0_SR2")
+    args = vars(parser.parse_args())
+
+    installer=jedox_installer(args)
